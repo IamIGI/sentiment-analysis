6cc1a588c4ef5d40b38192d1926bbb9536a52f4a
i.kłusek <J6ZkqwVUn>
2025-04-03 16:16:56 +0200

    SA-16- Add modalResult  RWD

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -31,4 +31,11 @@
 		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
 		border-radius: 10px;
 	}
+
+	@media (max-width: 700px) {
+		.modal {
+			padding-top: 0;
+			justify-content: center;
+		}
+	}
 </style>
